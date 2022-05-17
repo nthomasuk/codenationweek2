1274ea89eff94e925981bd73ba19b4c715547f94
ntcreationsuk <66379881+ntcreationsuk@users.noreply.github.com>
2022-05-17 13:18:49 +0100

    adds static nav bar and 2 new pages

--- daily.js
+++ daily.js
@@ -8,7 +8,21 @@
 
 // backwards('hello');
 
-function backwards(str) {
-    console.log(str.split("").reverse().join(""));
-}
-backwards("hello");
+// function backwards(str) {
+//     console.log(str.split("").reverse().join(""));
+// }
+// backwards("hello");
+
+const stringArr = [
+    "a",
+    "aa",
+    "aaa",
+    "aaaaa",
+    "aaaa"
+]
+
+longestString = stringArr.reduce((a, b) => a.length > b.length ? a : b, "");
+console.log(longestString); 
+
+
+
